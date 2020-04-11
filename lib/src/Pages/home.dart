a6d2887d2c99a52238e169d1c4144a286eb8d512
goandreus <andreshtml5@gmail.com>
2020-04-11 16:58:56 -0500

    page liquid callback

--- lib/src/Pages/home.dart
+++ lib/src/Pages/home.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:liquid/src/widgets/liquid_page.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
 
 class HomePape extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Hola mundo"),
+      body: LiquidSwipe( 
+        pages: liquidPages,
+        fullTransitionValue: 300,
+        enableLoop: true,
+        enableSlideIcon: true,
+        positionSlideIcon: 0.8,
+        waveType: WaveType.liquidReveal,
+
+  //in the event that the application breaks
+        onPageChangeCallback: (page) => pageChangeCallBack(page),
+        currentUpdateTypeCallback: (updateType) => updateTypeCallback(updateType),
       ),
     );
+  }
+
+  pageChangeCallBack(int page){
+    print(page);
+  }
+
+  updateTypeCallback(UpdateType updateType){
+    print(updateType);
   }
 }
---
+++ lib/src/widgets/liquid_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+final styleText = TextStyle(fontSize: 20.0, color: Colors.white);
+
+final liquidPages = [
+  Container(
+    color: Colors.pinkAccent,
+    child: Center(
+      child: Text(
+        'Text 1', style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.purpleAccent,
+    child: Center(
+      child: Text(
+        'Text 2', style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.greenAccent,
+    child: Center(
+      child: Text(
+        'Text 3', style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.blueAccent,
+    child: Center(
+      child: Text(
+        'Text 4', style: styleText,
+      ),
+    ),
+  ),
+  Container(
+    color: Colors.amberAccent,
+    child: Center(
+      child: Text(
+        'Text 5', style: styleText,
+      ),
+    ),
+  ),
+];
